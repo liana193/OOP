23322f040927acea4a32e179358d3ec24557b51d
Liana Najaroen <39064975+liana193@users.noreply.github.com>
2018-07-13 10:28:31 -0400

    Update token.cpp

--- token.cpp
+++ token.cpp
@@ -39,7 +39,7 @@ Token get_token(){
     case '%':
     return Token{ch};
 
-    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
         {
            cin.putback(ch);
            double val;
